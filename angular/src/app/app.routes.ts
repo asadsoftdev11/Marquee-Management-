19dcb7ba69780d27740fa918e0deaa42221366ea
Asad Babar <asadbabar11@yahoo.com>
2026-06-29 20:41:26 +0500

    fixed

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -15,57 +15,57 @@ export const APP_ROUTES: Routes = [
   },
   {
     path: 'identity',
-    canActivate: [authGuard],
+    //canActivate: [authGuard],
     loadChildren: () => import('@abp/ng.identity').then(c => c.createRoutes()),
   },
   {
     path: 'tenant-management',
-    canActivate: [authGuard],
+    //canActivate: [authGuard],
     loadChildren: () => import('@abp/ng.tenant-management').then(c => c.createRoutes()),
   },
   {
     path: 'setting-management',
-    canActivate: [authGuard],
+    //canActivate: [authGuard],
     loadChildren: () => import('@abp/ng.setting-management').then(c => c.createRoutes()),
   },
 {
   path: 'marquees',
-    canActivate: [authGuard],
+   // canActivate: [authGuard],
   loadComponent: () => import('./marquees/marquees').then(c => c.Marquees),
 },
 {
   path: 'customers',
-    canActivate: [authGuard],
+  //  canActivate: [authGuard],
   loadComponent: () => import('./customers/customers').then(c => c.Customers),
 },
 {
   path: 'menu-items',
-    canActivate: [authGuard],
+   // canActivate: [authGuard],
   loadComponent: () => import('./menu-items/menu-items').then(c => c.MenuItems),
 },
 {
   path: 'menu-categories',
-    canActivate: [authGuard],
+   // canActivate: [authGuard],
   loadComponent: () => import('./menu-categories/menu-categories').then(c => c.MenuCategories),
 },
 {
   path: 'bookings',
-    canActivate: [authGuard],
+   // canActivate: [authGuard],
   loadComponent: () => import('./bookings/bookings').then(c => c.Bookings),
 },
 {
   path: 'booking-menu-options',
-    canActivate: [authGuard],
+   // canActivate: [authGuard],
   loadComponent: () => import('./booking-menu-options/booking-menu-options').then(c => c.BookingMenuOptions),
 },
 {
   path: 'about-marquee',
-    canActivate: [authGuard],
+  //  canActivate: [authGuard],
   loadComponent: () => import('./pages/about-marquee/about-marquee') .then(m => m.AboutMarquee),
 },
 {
   path: 'track-bookings',
-    canActivate: [authGuard],
+  // canActivate: [authGuard],
   loadComponent: () => import('./track-bookings/track-bookings').then(c => c.TrackBookings),
 }
 
