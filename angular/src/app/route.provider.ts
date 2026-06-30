b08dc0e18012c0f9767c545a51963311bfd33ffa
Asad Babar <asadbabar11@yahoo.com>
2026-06-30 17:25:44 +0500

    login issue

--- angular/src/app/route.provider.ts
+++ angular/src/app/route.provider.ts
@@ -29,7 +29,7 @@ function configureRoutes() {
       path: '/marquees',
       name: '::Menu:Marquees',
       iconClass: 'fas fa-warehouse',
-    //  parentName: '::Menu:MarqueeManagement',
+      //  parentName: '::Menu:MarqueeManagement',
       order: 1,
       layout: eLayoutType.application,
       requiredPolicy: 'MarqueeManagement.Marquees',
@@ -38,43 +38,43 @@ function configureRoutes() {
       path: '/customers',
       name: '::Menu:Customers',
       iconClass: 'fas fa-users',
-     // parentName: '::Menu:MarqueeManagement',
+      // parentName: '::Menu:MarqueeManagement',
       order: 2,
       layout: eLayoutType.application,
       requiredPolicy: 'MarqueeManagement.Customers',
     },
-   {
+    {
       path: '/menu-categories',
       name: '::Menu:MenuCategories',
       iconClass: 'fas fa-list-alt',
-     // parentName: '::Menu:MarqueeManagement',
+      // parentName: '::Menu:MarqueeManagement',
       order: 3,
       layout: eLayoutType.application,
       requiredPolicy: 'MarqueeManagement.MenuCategories',
     },
-  {
+    {
       path: '/menu-items',
       name: '::Menu:MenuItems',
       iconClass: 'fas fa-utensils',
-    //  parentName: '::Menu:MarqueeManagement',
+      //  parentName: '::Menu:MarqueeManagement',
       order: 4,
       layout: eLayoutType.application,
       requiredPolicy: 'MarqueeManagement.MenuItems',
     },
-   {
+    {
       path: '/bookings',
       name: '::Menu:Bookings',
       iconClass: 'fas fa-calendar-check',
-    //  parentName: '::Menu:MarqueeManagement',
+      //  parentName: '::Menu:MarqueeManagement',
       order: 5,
       layout: eLayoutType.application,
       requiredPolicy: 'MarqueeManagement.Bookings',
     },
-  {
+    {
       path: '/booking-menu-options',
       name: '::Menu:BookingMenuOptions',
       iconClass: 'fas fa-concierge-bell',
-     // parentName: '::Menu:MarqueeManagement',
+      // parentName: '::Menu:MarqueeManagement',
       order: 6,
       layout: eLayoutType.application,
       requiredPolicy: 'MarqueeManagement.BookingMenuOptions',
@@ -86,12 +86,12 @@ function configureRoutes() {
       order: 8,
       layout: eLayoutType.application,
     },
- {
-  path: '/track-bookings',
-  name: '::Menu:BookingTrack',
-  iconClass: 'fas fa-search',
-  order: 6.5,
-  layout: eLayoutType.application,
-}
+    {
+      path: '/track-bookings',
+      name: '::Menu:BookingTrack',
+      iconClass: 'fas fa-search',
+      order: 6.5,
+      layout: eLayoutType.application,
+    },
   ]);
 }
